483c1753be97050d8ffc62025adfa12782fbb331
Luis Alberto Garrido Mendoza <lagarridom@gmail.com>
2018-09-08 20:52:37 -0500

    Update archivos2.py

--- Archivos/archivos2.py
+++ Archivos/archivos2.py
@@ -1,22 +1,24 @@
-# En modo de apertura a
+# En modo de apertura 'a'
 # puntero inicia al final del archivo
 
 #archivo = open('arch2.txt','a')
-archivo = open('arch2.txt','a+')
+archivo = open('arch2.txt','a+') # Podemos escribir y leer
 # append siempre escribe al final
 archivo.write('Otro Final\n')
 texto1 = archivo.read()
-print(texto1) # Ver que no imprime nada
+print(texto1) # No imprime nada porque el
+# puntero está hasta el final
 
+
+# Movemos puntero al inicio
 archivo.seek(0)
 texto2 = archivo.read()
-print(texto2)
+print(texto2) # Imprime texto del archivo
+
 
 archivo.seek(0)
-print(archivo.readlines())
-
-archivo.seek(0)
-lineas = archivo.readlines()
+lineas = archivo.readlines() # Creamos lista con líneas de archivo
+print(lineas)
 print(lineas[0])
 
 archivo.close()
