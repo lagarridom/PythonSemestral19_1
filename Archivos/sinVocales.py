702d3d07387ae464136f9caa9c7d4ab63bc7739c
Luis Alberto Garrido Mendoza <lagarridom@gmail.com>
2018-09-08 20:53:01 -0500

    Update sinVocales.py

--- Archivos/sinVocales.py
+++ Archivos/sinVocales.py
@@ -1,15 +1,35 @@
+# Código que permite que sobreescribe contenido de un archivo
+# pero con minúsculas
+
+# Pedimos nombre de archivio (arch1.txt)
 nombre = input("Ingresa el nombre del archivo: ")
 
-vocales = ('a','e','i','o','u','á')
+# Creamos tupla con vocales
+vocales = ('a','e','i','o','u')
+
+# Creamos objeto archivo de lectura
 lectura = open(nombre)
+# Guardamos texto de archivo
 texto = lectura.read()
-#print(texto)
 lectura.close()
+
+
 textoNuevo = ""
+
+'''
+Por cada caracter en el texto, si el caracter se
+encuentra en la tupla de vocales, no vamos a escribir
+ese caracter en textoNuevo, en caso contrario, lo vamos
+a escribir en textoNuevo
+'''
 for caracter in texto:
 	if caracter in vocales:
 		caracter = ""
 	textoNuevo += caracter
 
+# Creamos objeto archivo de escritura y borramos
+# el contenido que tenía el archivo que abrimos
 escritor = open(nombre,'w')
+
+# Escribimos textoNuevo en el
 escritor.write(textoNuevo)
