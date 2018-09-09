64b4ab50b62a85929cfc49733a7c51d7a5f4ac49
Luis Alberto Garrido Mendoza <lagarridom@gmail.com>
2018-09-08 20:52:04 -0500

    Update archivos1.py

--- Archivos/archivos1.py
+++ Archivos/archivos1.py
@@ -10,6 +10,10 @@
 # + -> Permite que el archivo sea tanto de escritura
 #	   como de lectura ("w+","r+","a+")
 
+
+# Antes de poder usar este código necesitamos
+# crear un archivo con texto de prueba llamado 
+# arch1.txt
 archivo1 = open("arch1.txt")
 # archivo = open("prueba1.txt","r")
 
@@ -17,17 +21,18 @@ archivo1 = open("arch1.txt")
 # en modo de lectura
 texto = archivo1.read()
 print(texto)
-# archivo.write("Otra cosa")
+# archivo.write("Otra cosa") -> Error
+
 
 archivo2 = open("arch2.txt",'w')
 # Creamos o sobreescribimos archivo llamado
 # 'prueba2.txt' en modo de escritura
 
-# Explicar un poco de métodos de apertura
+
 texto2 = "Texto 2\nSigo sin saber\nque decir\n"
 archivo2.write(texto2)
-archivo2.write("Final\n") # Para que vean lo del puntero
-#archivo2.read()
+archivo2.write("Final\n")
+#archivo2.read() -> Error
 # En general, cuando acabemos de trabajar con un
 # archivo guardamos y cerramos
 
