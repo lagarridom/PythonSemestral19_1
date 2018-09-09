91d25fa14e68ce4de9df54f21b25c6882fc40759
Luis Alberto Garrido Mendoza <lagarridom@gmail.com>
2018-09-08 21:02:29 -0500

    Update acumulador.py

--- ControlFlujo/acumulador.py
+++ ControlFlujo/acumulador.py
@@ -3,10 +3,17 @@
 
 suma = 0
 
+'''
+En un loop infinito vamos a pedirle un número entero al usuario,
+cada uno de estos números los vamos a estar sumando a la variables
+suma. Cuando el usuario ingrese como número el 0, vamos a salir
+del loop infinito
+'''
 while True:
 	numero = int(input("Dame el número que quieres sumar: "))
 	if numero == 0:
 		break
 	suma += numero
 
+# Imprimimos la suma
 print("La suma es: "+ str(suma))
