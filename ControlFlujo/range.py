a24107295c4ebe68fe3788bd0325ad2c6c37664b
Luis Alberto Garrido Mendoza <lagarridom@gmail.com>
2018-09-08 21:04:03 -0500

    Update range.py

--- ControlFlujo/range.py
+++ ControlFlujo/range.py
@@ -1,12 +1,17 @@
 # 12.-
-for i in range(10):
+
+# Del 0 al 9
+for i in range(10): 
 	print(i)
 
+# del 50 al 59
 for i in range(50,60):
 	print(i)
 
+# del 10 al 23 de 2 en 2
 for i in range(10,24,2):
 	print(i)
 
+# del 16 al 11 de -2 en -2
 for i in range(16,10,-2):
 	print(i)
